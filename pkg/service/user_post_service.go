abe13bda3c879ea5b2629b1b7fc9e4ed8baa4100
Matheus Lucca do Carmo <mematheuslc@gmail.com>
2022-11-05 05:02:22 -0300

    Use the power of go:generate (#2)
    * Start to generate some code trough open api
    
    * Use go generate to generate source code

--- pkg/service/user_post_service.go
+++ pkg/service/user_post_service.go
@@ -1,5 +1,7 @@
 package service
 
+//go:generate mockgen -source=./user_post_service.go -destination=../mock/service/user_post_service.go
+
 import (
 	"gosocialgraph/pkg/entity"
 	"gosocialgraph/pkg/repository"
